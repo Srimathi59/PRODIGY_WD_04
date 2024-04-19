ebe306510b1e738a8bf45fe944a20177fd606ccc
Srimathi59 <142609772+Srimathi59@users.noreply.github.com>
2024-04-19 21:07:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","Mernstack Developer","UI/UX Designer","Java Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
